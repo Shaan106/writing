cc9f01cba3b242779787e6d8b15e734ca648b9ad
Shaan Yadav <ay140@duke.edu>
2025-01-25 18:26:50 -0500

    acc graphs

---
+++ EMBC_graphs/model_accuracy.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# Data for the bar plot
+models = ['Shiao et al.', 'NEO']
+accuracies = [89, 75]
+
+# Create the bar plot with transparent colors and thin boundary lines
+plt.bar(models, accuracies, color=['orange', 'lightskyblue'], edgecolor='black', alpha=0.7)
+
+# Add gridlines
+plt.grid(axis='y', linestyle='--', alpha=0.6)
+
+# Add labels and title
+plt.xlabel('Model')
+plt.ylabel('Accuracy (%)')
+plt.title('Accuracy of Seizure Detection Models')
+
+
+# Save the plot
+plt.savefig('plots/model_accuracy.png')
